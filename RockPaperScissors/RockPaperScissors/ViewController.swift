ba7bdfdb7c5480dfe0dcaab44500a82568affa32
rami-mustafa <62889691+rami-mustafa@users.noreply.github.com>
2023-06-26 21:15:59 +0200

    Create a project

---
+++ RockPaperScissors/RockPaperScissors/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RockPaperScissors
+//
+//  Created by Ghaiath Alhereh on 26.06.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
